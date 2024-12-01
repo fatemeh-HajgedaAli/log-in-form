b14f3d24a15ef0f92179727659a13a663ffe98d6
fatemeh-HajgedaAli <fatemeh.hajgedaali1996@gmail.com>
2024-12-01 11:54:13 +0330

    codding

--- design/javaScript.js
+++ design/javaScript.js
@@ -1,9 +1,45 @@
 let userNameInput=document.querySelector('#userName')
 let passwordInput=document.querySelector('#userPassword')
+let textPassword=document.querySelector('.textPassword')
+let textUsername=document.querySelector('.textUsername')
 let alarmText=document.querySelector('.modalText')
-
 let loginForm=document.querySelector('.logIn')
 
+function usernameValidation(){
+    let userNameValue=userNameInput.value
+
+ if(userNameValue.length<12){
+
+     textUsername.style.display='inline'
+ }else{
+     textUsername.innerHTML="that's nice!" 
+     textUsername.style.color='green' 
+     textUsername.style.display='inline'  
+ }
+ setTimeout(function(){
+    textUsername.style.display='none' 
+},3000);
+}
+function passwordValidation(){
+
+let passwordValue=passwordInput.value
+
+ if(passwordValue.length<8){
+
+textPassword.style.display='inline'
+
+ }else{
+
+textPassword.innerHTML="that's true!" 
+textPassword.style.color='green' 
+textPassword.style.display='inline'  
+ }
+
+ setTimeout(function(){
+    textPassword.style.display='none' 
+},3000);
+
+}
 
 function clickBtn(){
 
